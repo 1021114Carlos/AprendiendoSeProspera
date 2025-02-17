400a72ce7720a7038f80dc6711bcbfe3575b2ab0
1021114Carlos <36316304+1021114Carlos@users.noreply.github.com>
2025-02-16 20:07:00 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,18 +1,3 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const tiles = document.querySelectorAll('.tile');
-
-    tiles.forEach(tile => {
-        tile.addEventListener('mouseenter', () => {
-            tile.style.transform = 'scale(1.05)';
-        });
-
-        tile.addEventListener('mouseleave', () => {
-            tile.style.transform = 'scale(1)';
-        });
-
-        tile.addEventListener('click', () => {
-            const sectionId = tile.querySelector('a').getAttribute('href');
-            window.location.href = sectionId;
-        });
-    });
-});
+function goToSection(section) {
+    window.location.href = section + ".html";
+}
